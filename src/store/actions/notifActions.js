98e4fe5af67e52badf7b15e4ff10b36be9222e3b
Adrian <adrian.domanski.dev@gmail.com>
2019-09-04 08:03:22 +0200

    Deploy version better time calc

--- src/store/actions/notifActions.js
+++ src/store/actions/notifActions.js
@@ -1,41 +1,52 @@
 export const createNotif = notif => {
-    return (dispatch, getState, { getFirestore }) => {
-        const firestore = getFirestore();
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
 
-        firestore.collection('notifications').add({
-            ...notif
-        }).then(() => {
-            dispatch({ type: 'CREATE_NOTIF_SUCCESS' })
-        })
-    }
-}
+    firestore
+      .collection("notifications")
+      .add({
+        ...notif
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_NOTIF_SUCCESS" });
+      });
+  };
+};
 
 export const deleteAllOutDatedNotifs = () => {
-    return (dispatch, getState, { getFirestore }) => {
-        const now = new Date();
-        const firestore = getFirestore();
-        const getData = async () => {
-            const snapshot = await firestore.collection('notifications').get()
-            snapshot.docs.forEach(doc => {
-                const notif_id = doc.id;
-                const notif = doc.data();
-                const difference = notif.date.toDate() - now;
-                if (difference < -100000000) {
-                    firestore.collection('notifications').doc(notif_id).delete()
-                        .then(() => { dispatch({ type: 'NOTIF_OUTDATE_DELETED' }) })
-                }
-            })
+  return (dispatch, getState, { getFirestore }) => {
+    const now = new Date();
+    const firestore = getFirestore();
+    const getData = async () => {
+      const snapshot = await firestore.collection("notifications").get();
+      snapshot.docs.forEach(doc => {
+        const notif_id = doc.id;
+        const notif = doc.data();
+        const difference = notif.date.toDate() - now;
+        if (difference < -86400000) {
+          firestore
+            .collection("notifications")
+            .doc(notif_id)
+            .delete()
+            .then(() => {
+              dispatch({ type: "NOTIF_OUTDATE_DELETED" });
+            });
         }
-        getData();
-    }
-}
+      });
+    };
+    getData();
+  };
+};
 
 export const deleteNotif = notif => {
-    return (dispatch, getState, { getFirestore }) => {
-        const firestore = getFirestore();
-        firestore.collection('notifications').doc(notif.id).delete()
-            .then(() => {
-                dispatch({ type: 'NOTIF_DELETED_SUCCESS' })
-            })
-    }
-}
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection("notifications")
+      .doc(notif.id)
+      .delete()
+      .then(() => {
+        dispatch({ type: "NOTIF_DELETED_SUCCESS" });
+      });
+  };
+};
--- src/store/actions/taskActions.js
+++ src/store/actions/taskActions.js
@@ -1,45 +1,58 @@
 export const createTask = task => {
-    return (dispatch, getState, { getFirestore }) => {
-        const firestore = getFirestore();
-        firestore.collection('tasks').add({
-            ...task
-        }).then(() => {
-            dispatch({ type: 'CREATE_TASK_SUCCESS', task })
-        }).catch(err => {
-            dispatch({ type: 'CREATE_TASK_ERROR' }, err);
-        })
-    }
-}
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection("tasks")
+      .add({
+        ...task
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_TASK_SUCCESS", task });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_TASK_ERROR" }, err);
+      });
+  };
+};
 
 export const deleteTask = task => {
-    return (dispatch, getState, { getFirestore }) => {
-        const firestore = getFirestore();
-        firestore.collection('tasks').doc(task.id).delete()
-            .then(() => {
-                dispatch({ type: 'DELETE_TASK_SUCCESS' })
-            }).catch(err => {
-                dispatch({ type: 'DELETE_TASK_ERROR' }, err)
-            })
-    }
-}
+  return (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    firestore
+      .collection("tasks")
+      .doc(task.id)
+      .delete()
+      .then(() => {
+        dispatch({ type: "DELETE_TASK_SUCCESS" });
+      })
+      .catch(err => {
+        dispatch({ type: "DELETE_TASK_ERROR" }, err);
+      });
+  };
+};
 
 export const deleteAllOutDatedTasks = () => {
-    return (dispatch, getState, { getFirestore }) => {
-        const now = new Date();
-        const firestore = getFirestore();
-        const getData = async () => {
-            const snapshot = await firestore.collection('tasks').get()
-            snapshot.docs.forEach(doc => {
-                const task_id = doc.id;
-                const task = doc.data();
-                const difference = task.date.toDate() - now;
-                if (difference < -100000000) {
-                    firestore.collection('tasks').doc(task_id).delete()
-                        .then(() => { dispatch({ type: 'TASK_OUTDATE_DELETED' }) })
-                }
-            })
+  return (dispatch, getState, { getFirestore }) => {
+    const now = new Date();
+    const firestore = getFirestore();
+    const getData = async () => {
+      const snapshot = await firestore.collection("tasks").get();
+      snapshot.docs.forEach(doc => {
+        const task_id = doc.id;
+        const task = doc.data();
+        const difference = task.date.toDate() - now;
+        if (difference < -86400000) {
+          firestore
+            .collection("tasks")
+            .doc(task_id)
+            .delete()
+            .then(() => {
+              dispatch({ type: "TASK_OUTDATE_DELETED" });
+            });
         }
+      });
+    };
 
-        getData();
-    }
-}
+    getData();
+  };
+};
